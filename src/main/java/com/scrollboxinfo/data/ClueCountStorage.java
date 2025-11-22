c63259c6c011984a53bccad4abbdf421456ce884
IEarnSolo <rlgithub@yahoo.com>
2025-11-22 00:40:43 -0500

    Modify saving and loading of clue counts and config settings to work per-account per-world type Add changelog system with changelog.md Remove unused legacy keys from plugin config and rsprofile Modify build.gradle to display correct plugin group and version

--- src/main/java/com/scrollboxinfo/data/ClueCountStorage.java
+++ src/main/java/com/scrollboxinfo/data/ClueCountStorage.java
@@ -24,28 +24,6 @@ public class ClueCountStorage
     public void setCount(ClueTier tier, int count)
     {
         clueCounts.put(tier, count);
-
-        switch (tier)
-        {
-            case BEGINNER:
-                config.setTotalBeginner(count);
-                break;
-            case EASY:
-                config.setTotalEasy(count);
-                break;
-            case MEDIUM:
-                config.setTotalMedium(count);
-                break;
-            case HARD:
-                config.setTotalHard(count);
-                break;
-            case ELITE:
-                config.setTotalElite(count);
-                break;
-            case MASTER:
-                config.setTotalMaster(count);
-                break;
-        }
     }
 
     public int getCount(ClueTier tier)
@@ -56,38 +34,6 @@ public class ClueCountStorage
     public void setBankCount(ClueTier tier, int count)
     {
         bankCounts.put(tier, count);
-
-        switch (tier)
-        {
-            case BEGINNER:
-                config.setBankedBeginner(count);
-                break;
-            case EASY:
-                config.setBankedEasy(count);
-                break;
-            case MEDIUM:
-                config.setBankedMedium(count);
-                break;
-            case HARD:
-                config.setBankedHard(count);
-                break;
-            case ELITE:
-                config.setBankedElite(count);
-                break;
-            case MASTER:
-                config.setBankedMaster(count);
-                break;
-        }
-    }
-
-    public void loadBankCountsFromConfig()
-    {
-        bankCounts.put(ClueTier.BEGINNER, config.bankedBeginner());
-        bankCounts.put(ClueTier.EASY, config.bankedEasy());
-        bankCounts.put(ClueTier.MEDIUM, config.bankedMedium());
-        bankCounts.put(ClueTier.HARD, config.bankedHard());
-        bankCounts.put(ClueTier.ELITE, config.bankedElite());
-        bankCounts.put(ClueTier.MASTER, config.bankedMaster());
     }
 
     public int getBankCount(ClueTier tier)
@@ -95,13 +41,4 @@ public class ClueCountStorage
         return bankCounts.getOrDefault(tier, 0);
     }
 
-    public void loadTotalCountsFromConfig()
-    {
-        clueCounts.put(ClueTier.BEGINNER, config.totalBeginner());
-        clueCounts.put(ClueTier.EASY, config.totalEasy());
-        clueCounts.put(ClueTier.MEDIUM, config.totalMedium());
-        clueCounts.put(ClueTier.HARD, config.totalHard());
-        clueCounts.put(ClueTier.ELITE, config.totalElite());
-        clueCounts.put(ClueTier.MASTER, config.totalMaster());
-    }
 }
---
+++ src/main/resources/changelog.md
@@ -0,0 +1,7 @@
+## 1.2.0
+- Plugin settings now save per-account per-world type.
+- Clue counts now save per-account per-world type.
+- Clue counts were reset - simply open your bank to track them again.
+
+## 1.0.0
+- Initial release
